e889661320f5bfcd1ec6b805b973bf31388083ed
GSacata <gsacata@gmail.com>
2025-02-01 17:19:01 -0300

    Using lit variables as temporary content storage.

--- src/main/java/com/example/news/controllers/CommentController.java
+++ src/main/java/com/example/news/controllers/CommentController.java
@@ -1,20 +1,41 @@
 package com.example.news.controllers;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.news.domain.Comment;
 import com.example.news.utils.Sample;
+import com.example.news.utils.Utils;
 
 @RestController
 @RequestMapping("comments")
 public class CommentController {
+
+    List<Comment> tempCommentList = new ArrayList<Comment>();
     
     @GetMapping("")
     public List<Comment> GetComments() {
+        if (!tempCommentList.isEmpty()) {
+            return tempCommentList;
+        }
         return Sample.sampleComments();
+    }
+
+    @PostMapping("/post-comment")
+    public Comment commentPost(@RequestBody Comment body) {
+        String author = body.getAuthor();
+        String content = body.getContent();
+        Instant publicationMoment = Utils.returnFormattedNow();
+
+        Comment comment = new Comment(author, content, publicationMoment);
+        this.tempCommentList.add(comment);
+        return comment;
     }
 }
--- src/main/java/com/example/news/controllers/NewsController.java
+++ src/main/java/com/example/news/controllers/NewsController.java
@@ -1,20 +1,42 @@
 package com.example.news.controllers;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.news.domain.News;
 import com.example.news.utils.Sample;
+import com.example.news.utils.Utils;
 
 @RestController
 @RequestMapping("/news")
 public class NewsController {
+
+    List<News> tempNewsList = new ArrayList<News>();
     
     @GetMapping("")
     public List<News> GetNews() {
+        if (!this.tempNewsList.isEmpty()) {
+            return this.tempNewsList;
+        }
         return Sample.sampleNews();
+    }
+
+    @PostMapping("write")
+    public News newsPost(@RequestBody News body) {
+        String headline = body.getHeadline();
+        String author = body.getAuthor();
+        String content = body.getContent();
+        Instant publicationMoment = Utils.returnFormattedNow();
+
+        News postedNew = new News(headline, author, content, publicationMoment);
+        this.tempNewsList.add(postedNew);
+        return postedNew;
     }
 }
